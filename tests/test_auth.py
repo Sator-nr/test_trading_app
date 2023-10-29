c10a36344c3bef666cbf49b31d4bb385523d1a35
sator_nr <sator_nr@mail.ru>
2023-10-30 03:40:32 +0400

    More tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,3 @@
-
-
 from sqlalchemy import insert, select
 from src.auth.models import role
 from tests.conftest import client, async_session_maker
@@ -27,3 +25,68 @@ def test_register():
       "role_id": 1
     })
     assert response.status_code == 201
+
+
+def test_auth_real():
+    headers = {
+        'accept': 'application/json',
+    }
+    data = {
+        'grant_type': '',
+        'username': 'string@string.string',
+        'password': 'string',
+        'scope': '',
+        'client_id': '',
+        'client_secret': '',
+    }
+    response = client.post("/auth/jwt/login", headers=headers, data=data)
+    assert response.status_code == 204
+
+
+def test_auth_fake_user():
+    headers = {
+        'accept': 'application/json',
+    }
+    data = {
+        'grant_type': '',
+        'username': 'fake@email.nope',
+        'password': 'just_nope',
+        'scope': '',
+        'client_id': '',
+        'client_secret': '',
+    }
+    response = client.post("/auth/jwt/login", headers=headers, data=data)
+    assert response.status_code == 400
+
+
+def test_auth_fake_email():
+    headers = {
+        'accept': 'application/json',
+    }
+    data = {
+        'grant_type': '',
+        'username': 'fake@email.nope',
+        'password': 'string',
+        'scope': '',
+        'client_id': '',
+        'client_secret': '',
+    }
+    response = client.post("/auth/jwt/login", headers=headers, data=data)
+    assert response.status_code == 400
+
+
+def test_auth_fake_password():
+    headers = {
+        'accept': 'application/json',
+    }
+    data = {
+        'grant_type': '',
+        'username': 'string@string.string',
+        'password': 'just_nope',
+        'scope': '',
+        'client_id': '',
+        'client_secret': '',
+    }
+    response = client.post("/auth/jwt/login", headers=headers, data=data)
+    assert response.status_code == 400
+
